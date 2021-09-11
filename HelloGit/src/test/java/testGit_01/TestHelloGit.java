99bc1c125fc26c77325719c4111cb14929c41247
hp_zy999 <hp_zy999@163.com>
2021-09-11 09:30:41 +0800

    测试代码块提交

---
+++ HelloGit/src/test/java/testGit_01/TestHelloGit.java
@@ -0,0 +1,23 @@
+package testGit_01;
+
+import com.hapy.git.HelloGit;
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+
+public class TestHelloGit {
+
+    @Test
+    public void Test1(){
+        Constructor<HelloGit> constructor;
+        HelloGit helloGit = null;
+        try {
+            constructor = HelloGit.class.getConstructor();
+            helloGit = constructor.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assert helloGit != null;
+        helloGit.helloGit();
+    }
+}
